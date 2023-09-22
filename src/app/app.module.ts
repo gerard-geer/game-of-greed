2d70ee67e6b5c3eff5edee0b2ac08a3b49f83de8
gerard <gerard@boopsboops.gerard>
2023-09-22 12:08:42 -0500

    Game state largely working, now need to implement scoreboard and player interaction.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { BoardCellComponent } from './components/board-cell/board-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    BoardCellComponent
   ],
   imports: [
     BrowserModule,
