d46d128a18f63a72eeb36ab029b0553d38b3dccf
gerard <gerard@boopsboops.gerard>
2023-09-22 15:52:33 -0500

    Set up for testing the greed-player component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
 import { BoardCellComponent } from './components/board-cell/board-cell.component';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
+import { GreedPlayerComponent } from './components/greed-player/greed-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    BoardCellComponent
+    BoardCellComponent,
+    ScoreboardComponent,
+    GreedPlayerComponent
   ],
   imports: [
     BrowserModule,
